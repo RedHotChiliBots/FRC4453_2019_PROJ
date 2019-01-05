c5273c2fca061015c767d36658704b36f308a2d4
Conner Ebbinghaus <connerebbinghaus@gmail.com>
2019-01-05 13:50:22 -0500

    Begin migrating code.

--- Robot/src/main/java/org/usfirst/frc/team4453/robot/library/Functions.java
+++
@@ -1,8 +0,0 @@
-package org.usfirst.frc.team4453.robot.library;
-
-public class Functions {
-
-    public static float clamp(float val, float min, float max) {
-        return Math.max(min, Math.min(max, val));
-    }
-}
