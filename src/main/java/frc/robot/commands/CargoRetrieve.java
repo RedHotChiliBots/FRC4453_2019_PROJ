4ed744eb1795df7fbbf5d7b6d735df819a3a06ec
Conner Ebbinghaus <connerebbinghaus@gmail.com>
2019-02-02 14:13:10 -0800

    Made Cargo Retrieve command group

--- src/main/java/frc/robot/commands/CargoRetrieve.java
+++ src/main/java/frc/robot/commands/CargoRetrieve.java
@@ -30,5 +30,7 @@ public class CargoRetrieve extends CommandGroup {
     // e.g. if Command1 requires chassis, and Command2 requires arm,
     // a CommandGroup containing them would require both the chassis and the
     // arm.
+    addSequential(new LiftGoToLoadingStation());
+    addSequential(new CargoGrab());
   }
 }
